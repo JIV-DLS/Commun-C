09c72a6c2a79f91c69da6011dede0bda7f1672a9
jiv <aniglojonas@gmail.com>
2019-09-15 12:10:28 +0000

    dans l'exo 2, negatif au lieu de négatif pour éviter les caractères bizarres à l'affichage

--- exo2.c
+++ exo2.c
@@ -2,7 +2,7 @@
 void exo2()
 {
     int i,nb=0;
-    puts("entrer un entier négatifs pour arreter la saisie: ");
+    puts("entrer un entier negatifs pour arreter la saisie: ");
     for(i=0;nb>=0;i++)
     {
         printf("Entrer un entier: ");
